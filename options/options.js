f132bd72499d98ae596a264b0016f9a064d0be79
Crowdbotics <team@crowdbotics.com>
2021-08-17 05:04:17 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,6 @@
+export const globalOptions = {
+  name: "late_lab_29873Identifier",
+  url: "https://late_lab_29873Identifier.botics.co",
+  api: "https://late_lab_29873Identifier.botics.co/api/v1"
+}
+export const modulesOptions = {}
